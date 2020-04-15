616f6556bf501d0f5cdd6e148c9db28f7b1a75b8
LaoQi <qjqjqzy@gmail.com>
2020-04-16 04:02:52 +0800

    调整序列化方案

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TFON
+
+* 使用protocol buffers做message
+* 使用[google/diff-match-patch](https://github.com/google/diff-match-patch.git)做文本对比
+* AES加密块
+* 对象存储保存数据
+* tk gui
--- frank.py
+++ frank.py
@@ -1,122 +1,111 @@
 """
-protocol
-{
-    big endian
-    header {
-        4 byte magic number 0x5f3759df
-        2 byte version
-        2 byte header length
-        4 byte data length
-        8 byte timestamp
-        32 byte sha-256 hash
-        ...
-    }
-    data
-}
+
 """
+import time
 import hashlib
-import struct
-import io
+from diff_match_patch import diff_match_patch
+from onekey_pb2 import MD5, SHA1, SHA256, SHA384, OneKey
 
-MAGIC_NUMBER = 0x5f3759df
+DEFAULT_HASH_METHOD = MD5
 
 
-class BlockParseError(Exception):
+class PhraseNotSet(Exception):
     pass
-
-
-class Block:
-    def __init__(self):
-        self.timestamp = 0
-        self.data = None
-        self.hash = None
-
-    @staticmethod
-    def get_version():
-        return 1
-
-    @staticmethod
-    def get_header_length():
-        # always 52
-        return 52
-
-    def length(self):
-        return self.get_header_length() + self.get_data_length()
-
-    def get_data_length(self):
-        if self.data:
-            return len(self.data)
-        else:
-            return 0
-
-    def data_hash(self) -> bytes:
-        sha = hashlib.sha256()
-        sha.update(self.data)
-        return sha.digest()
-
-    def get_data_hash(self) -> bytes:
-        if self.hash:
-            return self.hash
-        return self.data_hash()
-
-    def get_data(self) -> bytes:
-        if not self.data:
-            return b''
-        return self.data
-
-    def load(self, raw_data: bytes) -> int:
-        # return offset
-        if raw_data[:4] != MAGIC_NUMBER:
-            raise BlockParseError("not start with magic number!")
-        self.timestamp = int(struct.unpack(">Q", raw_data[12:20])[0])
-        self.hash = raw_data[20:52]
-        data_length = int(struct.unpack(">L", raw_data[8:12])[0])
-        self.data = raw_data[52:data_length]
-        return 52 + data_length
-
-    def dump(self) -> bytes:
-        header = struct.pack(">LHHLQs",
-                             MAGIC_NUMBER, self.get_version(), self.get_header_length(),
-                             self.get_data_length(), self.timestamp, self.get_data_hash())
-        return header + self.get_data()
 
 
 class Frank:
     def __init__(self):
         self._phrase = None
         self._blocks = []
-        self.raw_data = None
+        self.data = OneKey()
+        self.raw_data = self.data.SerializeToString()
+        self._patch = None
+        self._full_content = ""
+        self._is_changed = False
 
-    def password(self, value):
+    @property
+    def full_content(self):
+        if self._full_content:
+            return self._full_content
+        dmp = diff_match_patch()
+        content = ""
+        for block in self.data.blocks:
+            patches = dmp.patch_fromText(block.data)
+            content, _ = dmp.patch_apply(patches, content)
+        self._full_content = content
+        return content
+
+    def set_phrase(self, value):
         self._phrase = value
 
-    def encrypt(self):
-        if not self._phrase or not self.raw_data:
+    def update_content(self, value: str):
+        if self.full_content == value:
             return
+        self._is_changed = True
+        dmp = diff_match_patch()
+        patches = dmp.patch_make(self.full_content, value)
+        self._patch = dmp.patch_toText(patches)
+        print(self._patch)
+        self._full_content = value
+
+    def save(self):
+        if not self._is_changed or not self._patch:
+            return
+        if not self._phrase:
+            raise PhraseNotSet("phrase not set!")
+        block = self.data.blocks.add()
+        block.data = self._patch
+        block.timestamp = int(time.time())
+        block.method = DEFAULT_HASH_METHOD
+        block.hash = self._hash_patch()
+        self._patch = None
+        self._is_changed = False
+        self.encrypt()
+
+    def _hash_patch(self, method=DEFAULT_HASH_METHOD) -> bytes:
+        if not self._patch:
+            return b''
+        hash_methods = {
+            MD5: 'md5',
+            SHA1: 'sha1',
+            SHA256: 'sha256',
+            SHA384: 'sha384',
+        }
+        h = hashlib.new(hash_methods.get(method, 'md5'), self._patch.encode('utf8'))
+        return h.digest()
 
     def load(self, raw_data: bytes):
         self.raw_data = raw_data
         self.decrypt()
-        offset = 0
-        while True:
-            block = Block()
-            try:
-                offset = block.load(raw_data[offset:])
-                self._blocks.append(block)
-            except BlockParseError:
-                break
 
     def dumps(self):
-        buffer = io.BufferedIOBase()
-        for i in self._blocks:
-            buffer.write(i.dump())
-        return buffer.read()
+        return self.raw_data
+
+    def length(self):
+        return len(self.data.blocks)
+
+    def encrypt(self):
+        if not self._phrase:
+            return
+        self.raw_data = self.data.SerializeToString()
 
     def decrypt(self):
-        if not self._phrase or not self._blocks:
-            pass
+        if not self._phrase or not self.raw_data:
+            return
+        self.data.ParseFromString(self.raw_data)
 
 
 if __name__ == "__main__":
-    frank = Frank()
+    import os
 
+    frank = Frank()
+    frank.set_phrase("phrase")
+    if os.path.exists('tmp/save'):
+        with open('tmp/save', 'rb') as f:
+            frank.load(f.read())
+    frank.update_content("test 3\n this is \n new line")
+    frank.save()
+    print(frank.length())
+    with open('tmp/save', 'wb') as f:
+        f.write(frank.dumps())
+    print(frank.full_content)
